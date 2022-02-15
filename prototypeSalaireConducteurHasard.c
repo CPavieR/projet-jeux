a51e84271ff5da03846e9b69e26dfa7b55b50dff
MelukHa <98402950+MelukHa@users.noreply.github.com>
2022-02-15 15:45:08 +0100

    Update prototypeSalaireConducteurHasard.c
    ajout du chiffre d'affaire et la boucle du main de cette fonction en commentaire si besoin de faire des tests

--- prototypeSalaireConducteurHasard.c
+++ prototypeSalaireConducteurHasard.c
@@ -2,26 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-float revenu_au_hasard() {
+float cout_au_hasard() {
     float j=0;
-    j=(rand()%20);
+    j=(rand()%9)+7;
     return j/10.0;
 }
-
-
 
 char* nom_au_hasard() {
     char* noms[30] = {"Michel","Josué","Anguerrande","Josselin","Foulque","Frederic","David","Jean-Gertrude","Jépadnom","Liam",
     "William","Sophia","Nathan","Léo","Emma","Logan","Florence", "Thomas","Noah","Félix",
     "Edouard","Victor","Dylan","Louis","James","Ethan","Benjamin","Gabriel","Rose","Nolan"}; 
-    return noms[rand()%30+0];
+    return noms[rand()%30];
+}
+
+float chiffre_affaire(float cout_conducteur, int cpt_compteur_km) {
+    srand(time(NULL));
+    float multiplieur=(rand()%6)+13.0;
+    multiplieur = multiplieur/10.0;
+    return (cout_conducteur * cpt_compteur_km * multiplieur);
 }
 
 int main() {
-    srand(time(NULL));
-    for (int i=0; i <= 50; i++) {
-        char* nom = nom_au_hasard();
-        printf("le revenu sera de %f euro par kilomètre pour %s \n",revenu_au_hasard(),nom);
-    };
+    printf("%s coûtera %f euros par kilomètre et le contrat actuel rapportera %f à l'entreprise\n",nom_au_hasard(), cout_au_hasard(), chiffre_affaire(cout_au_hasard(),(rand()%300)+50));
+    //for (int i=0; i <= 20; i++) {
+        //printf("%s coûtera %f euros par kilomètre et le contrat actuel rapportera %f à l'entreprise\n",nom_au_hasard(), cout_au_hasard(), chiffre_affaire(cout_au_hasard(),(rand()%300)+50));
+    //};
     return 0;
 }
