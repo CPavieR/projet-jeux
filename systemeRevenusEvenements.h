87f8f971b7191e8fae29a1eb91ce3ecdb40b718e
MelukHa <98402950+MelukHa@users.noreply.github.com>
2022-02-22 15:39:52 +0100

    Update and rename coutEtRevenu.h to systemeRevenusEvenements.h
    ajout de la prise en charge des fonctions d'évènements aléatoires et des entreprises (contrats)

---
+++ systemeRevenusEvenements.h
@@ -0,0 +1,14 @@
+#ifndef SYSTEMEREVENUSEVENEMENTS
+#define SYSTEMEREVENUSEVENEMENTS
+#define NOMBRE_DE_VILLES   16
+
+
+float cout_au_hasard();
+char* nom_au_hasard();
+int prix_evenement_aleatoire(int n);
+char* evenement_aleatoire(int n);
+char* entreprise();
+int prix_contrat();
+void rnd_srand ();
+int int_random (void);
+#endif
