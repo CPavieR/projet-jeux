a94f7c17f6ed994c42b7a3ff62b7727d507deefd
Clément Pavie Routaboul <clement.pavieroutaboul@gmail.com>
2022-03-08 15:23:12 +0100

    test

--- dijkstra.h
+++ dijkstra.h
@@ -3,11 +3,4 @@
 #define NOMBRE_DE_VILLE 16
 int dijkstra(int matrice[NOMBRE_DE_VILLE][NOMBRE_DE_VILLE],int depart , int arrivee);
 
-<<<<<<< HEAD
 #endif
-=======
-_Bool is_not_in(int li[NOMBRE_DE_VILLE],int val);
-int dijkstra(int matrice[NOMBRE_DE_VILLE][NOMBRE_DE_VILLE],int depart , int arrivee);
-
-#endif
->>>>>>> 5cdc24d7733457b9fd209ab7eee5e3a193ab522c
