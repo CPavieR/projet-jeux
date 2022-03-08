333ad12ad029dfc45567526d562feea3f7576554
MelukHa <98402950+MelukHa@users.noreply.github.com>
2022-03-08 15:14:17 +0100

    Update dijkstra.h
    ajout du #endif et de la fonction is not in manquants

--- dijkstra.h
+++ dijkstra.h
@@ -1,4 +1,7 @@
 #ifndef DIJKSTRA_H
 #define DIJKSTRA_H
 
+_Bool is_not_in(int li[NOMBRE_DE_VILLE],int val);
 int dijkstra(int matrice[NOMBRE_DE_VILLE][NOMBRE_DE_VILLE],int depart , int arrivee);
+
+#endif
